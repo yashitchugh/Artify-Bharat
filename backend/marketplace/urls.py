7af55a2fb0f6578e5739413057935ae3b3b4a1cc
parth5012 <parthchawla5012@GMAIL.COM>
2026-01-25 17:23:55 +0530

    Added store app with data models and viewsets

--- backend/marketplace/urls.py
+++ backend/marketplace/urls.py
@@ -14,17 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
 
 
 if settings.DEBUG:
-    urlpatterns += debug_toolbar_urls
+    urlpatterns += debug_toolbar_urls()
