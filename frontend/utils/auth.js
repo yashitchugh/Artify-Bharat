853b47d2a71ca13fad697a40d4abbf749e9a3d40
parth5012 <parthchawla5012@GMAIL.COM>
2026-02-02 20:14:49 +0530

    Fixed the issues for refresh token

--- frontend/utils/auth.js
+++ frontend/utils/auth.js
@@ -8,45 +8,46 @@ export async function login(formData){
     })
     
     if (response.status == 200){
-        const token = response.access;
-        localStorage.setItem('authToken', token);
+        localStorage.setItem('access_token', response.access);
+        localStorage.setItem('refresh_token', response.refresh);
         console.log('Logged In Successfully!!')
     }
     else{
         console.log('Invalid Credentials!!')
-
+        
     }
 }
 
 export async function signup(formData,userRole){
     const data = {
-    userRole: userRole,
-    firstName: formData.firstName,
-    lastName: formData.lastName,
-    email: formData.email,
-    password: formData.password,
-    phone: formData.phone,
-    address: formData.address,
-    city: formData.city,
-    state: formData.state,
-    pincode: formData.pincode,
-    craftSpecialty: formData.craftSpecialty,
-    experience: formData.experience ,
-    bio: formData.bio ,
-    interests: formData.interests ,
-  };
+        userRole: userRole,
+        firstName: formData.firstName,
+        lastName: formData.lastName,
+        email: formData.email,
+        password: formData.password,
+        phone: formData.phone,
+        address: formData.address,
+        city: formData.city,
+        state: formData.state,
+        pincode: formData.pincode,
+        craftSpecialty: formData.craftSpecialty,
+        experience: formData.experience ,
+        bio: formData.bio ,
+        interests: formData.interests ,
+    };
   console.log(data);
-    const response =  await api.post('store/signup/',data)
-    const { token } = response.data;
-
-    // Save token to localStorage
-    localStorage.setItem('authToken', token);
-
-    if (response.status == 200){
-        console.log('Account Created Successfully!!');
+  const response =  await api.post('store/signup/',data)
+  const { token } = response.data;
+  
+  // Save token to localStorage
+  localStorage.setItem('access_token', response.access);
+  localStorage.setItem('refresh_token', response.refresh);
+  
+  if (response.status == 200){
+      console.log('Account Created Successfully!!');
     }
     else{
         console.log('Account could not be created!!');
-
+        
     }
 }
